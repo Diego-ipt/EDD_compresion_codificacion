7f5e55126600be8ac153ae82efaf12304b97db47
Antonia <montlopanto@gmail.com>
2024-06-20 19:34:13 -0400

    metodos_huffman convertido en clase con metodos estaticos

--- Huffman/metodos_huffman.h
+++ Huffman/metodos_huffman.h
@@ -13,103 +13,113 @@
 
 using namespace std;
 
-pair<string, Node*> codificar(string& mensaje) {
+class metodos_huffman {
 
-    // PriorityQueue cola_p;
-    priority_queue<Node*, vector<Node*>, Node::NodeComparator> cola_p;
-    unordered_map<char, int> frecuencia_simbolos;
-    unordered_map<char, Node*> hojas;
-    unordered_map<char, string> diccionario;
+    public:
+        static pair<string, Node*> codificar(string& mensaje) {
 
-    for(char simbolo : mensaje) {
-        frecuencia_simbolos[simbolo]++;
-    }
+            // PriorityQueue cola_p;
+            priority_queue<Node*, vector<Node*>, Node::NodeComparator> cola_p;
+            unordered_map<char, int> frecuencia_simbolos;
+            unordered_map<char, Node*> hojas;
+            unordered_map<char, string> diccionario;
 
-    for(auto& par : frecuencia_simbolos) {
-        Node* hoja = new Node(par.first, par.second);
-        cola_p.push(hoja);
-        hojas[par.first] = hoja;
-    }
-
-    //si el mensaje está compuesto de un solo simbolo, se codifica la cadena como puros 0, y retorna  el arbol huffman (cola.top)
-    if(cola_p.size() == 1) {
-        string bits = string(mensaje.length(), '0');
-        return make_pair(bits, cola_p.top());
-    }
-
-    while(cola_p.size() > 1) {
-        Node* u = cola_p.top();
-        cola_p.pop();
-        Node* v = cola_p.top();
-        cola_p.pop();
-        Node* nodo_comb = Node::combine(u, v);
-        cola_p.push(nodo_comb);
-        // cout << "nodo 1: " << u->getSymbol() << " "<<  u->getFrecuency() << endl;
-        // cout << "nodo 2: " <<  v->getSymbol() << " " << v->getFrecuency() << endl;
-        // cout << "nodo comb: " <<  nodo_comb->getFrecuency() << endl;
-
-
-    }
-
-    Node* tree = cola_p.top();
-    // cout << "nodo padre: " << tree->getSymbol() << " " <<  tree->getFrecuency() <<endl;
-
-    for(auto& hoja : hojas) {
-        string c;
-        Node* u = hoja.second;
-        //para cada hoja se obtiene su codigo binario
-        while (u != tree) {
-            Node* parent = u->getParent();  
-            if(u == parent->getLeft()) {
-                c += '0';
-            } else if (u == parent->getRight()) {
-                c += '1';
+            for(char simbolo : mensaje) {
+                frecuencia_simbolos[simbolo]++;
             }
-            u = parent;       
+
+            for(auto& par : frecuencia_simbolos) {
+                Node* hoja = new Node(par.first, par.second);
+                cola_p.push(hoja);
+                hojas[par.first] = hoja;
+            }
+
+            //si el mensaje está compuesto de un solo simbolo, se codifica la cadena como puros 0, y retorna  el arbol huffman (cola.top)
+            if(cola_p.size() == 1) {
+                string bits = string(mensaje.length(), '0');
+                return make_pair(bits, cola_p.top());
+            }
+
+            while(cola_p.size() > 1) {
+                Node* u = cola_p.top();
+                cola_p.pop();
+                Node* v = cola_p.top();
+                cola_p.pop();
+                Node* nodo_comb = Node::combine(u, v);
+                cola_p.push(nodo_comb);
+                // cout << "nodo 1: " << u->getSymbol() << " "<<  u->getFrecuency() << endl;
+                // cout << "nodo 2: " <<  v->getSymbol() << " " << v->getFrecuency() << endl;
+                // cout << "nodo comb: " <<  nodo_comb->getFrecuency() << endl;
+
+
+            }
+
+            Node* tree = cola_p.top();
+            // cout << "nodo padre: " << tree->getSymbol() << " " <<  tree->getFrecuency() <<endl;
+
+            for(auto& hoja : hojas) {
+                string c;
+                Node* u = hoja.second;
+                //para cada hoja se obtiene su codigo binario
+                while (u != tree) {
+                    Node* parent = u->getParent();  
+                    if(u == parent->getLeft()) {
+                        c += '0';
+                    } else if (u == parent->getRight()) {
+                        c += '1';
+                    }
+                    u = parent;       
+                }
+
+                reverse(c.begin(), c.end());
+                diccionario[hoja.first] = c;
+                // cout << hoja.first << ": " << c << endl;
+            }
+
+
+            string mensaje_cod;
+
+            for(char c : mensaje) {
+                mensaje_cod += diccionario[c];
+            }
+
+            //se retorna el mensaje codificado, junto con el puntero a la raiz del arbol
+            return make_pair(mensaje_cod, tree); 
+
         }
 
-        reverse(c.begin(), c.end());
-        diccionario[hoja.first] = c;
-        // cout << hoja.first << ": " << c << endl;
-    }
+        static string decodificar(string& mensaje_cod, Node* tree) {
+            string mensaje_decod;
+            if(tree->is_leaf()) {
+                mensaje_decod = string(mensaje_cod.length(), tree->getSymbol());
+                return mensaje_decod;
+            }
 
+            Node* v = tree;
 
-    string mensaje_cod;
+            for(char c : mensaje_cod) {
+                if(c == '0') {
+                    v = v->getLeft();
+                } else {
+                    v = v->getRight();
+                }
 
-    for(char c : mensaje) {
-        mensaje_cod += diccionario[c];
-    }
+                if(v->is_leaf()) {
+                    mensaje_decod += v->getSymbol();
+                    v = tree;
+                }
+            }
 
-    //se retorna el mensaje codificado, junto con el puntero a la raiz del arbol
-    return make_pair(mensaje_cod, tree); 
-
-}
-
-
-string decodificar(string& mensaje_cod, Node* tree) {
-    string mensaje_decod;
-    if(tree->is_leaf()) {
-        mensaje_decod = string(mensaje_cod.length(), tree->getSymbol());
-        return mensaje_decod;
-    }
-
-    Node* v = tree;
-
-    for(char c : mensaje_cod) {
-        if(c == '0') {
-            v = v->getLeft();
-        } else {
-            v = v->getRight();
+            return mensaje_decod;
         }
 
-        if(v->is_leaf()) {
-            mensaje_decod += v->getSymbol();
-            v = tree;
+        static size_t sizeBits_mensaje_comp(string& mensaje_comp) {
+            return mensaje_comp.size();
         }
-    }
 
-    return mensaje_decod;
-}
+
+};
+
 
 
 #endif
--- Huffman/testHuffman.cpp
+++ Huffman/testHuffman.cpp
@@ -7,17 +7,19 @@ using namespace std;
 int main() {
     string cadena = "tangananica-tanganana";
 
-    auto codificado = codificar(cadena);
+    auto codificado = metodos_huffman::codificar(cadena);
 
     string mensaje_cod = codificado.first;
 
-    auto decodificado = decodificar(mensaje_cod, codificado.second);
+    auto decodificado = metodos_huffman::decodificar(mensaje_cod, codificado.second);
 
     string mensaje_decod = decodificado;
 
     cout << "mensaje original: " << cadena << endl;
     cout << "mensaje codificado: " << mensaje_cod << endl;
     cout << "mensaje decodificado: " << mensaje_decod << endl;
+    cout << "size bits mensaje cod: " << cadena.size() * 8 << endl;
+    cout << "size bits mensaje_decod: " << metodos_huffman::sizeBits_mensaje_comp(mensaje_cod) << endl;
 
     return 0;
 }
