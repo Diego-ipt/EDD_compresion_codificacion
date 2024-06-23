66e3e9f01e70909570756027757a01d189408f8d
Antonia <montlopanto@gmail.com>
2024-06-23 00:09:23 -0400

    documentacion de huffman lista

--- Huffman/Node.h
+++ Huffman/Node.h
@@ -1,73 +1,128 @@
 #ifndef NODE_H
 #define NODE_H
 
+/*
+    Clase que representa los nodos del árbol de huffman
+*/
 class Node {
+private:
+    char symbol;       // Símbolo que contiene el nodo
+    int frecuency;     // Frecuencia del símbolo
+    Node* right;       // Puntero al hijo derecho
+    Node* left;        // Puntero al hijo izquierdo
+    Node* parent;      // Puntero al nodo padre
 
-    private:
-        char symbol;
-        int frecuency; 
-        Node* right;
-        Node* left;
-        Node* parent;
+public:
+    /**
+     * Constructor de la clase Node.
+     * 
+     * @param symbol El símbolo que este nodo representa.
+     * @param frecuency La frecuencia del símbolo.
+     * @param right Puntero al hijo derecho.
+     * @param left Puntero al hijo izquierdo.
+     */
+    Node(char symbol = '\0', int frecuency = 0, Node* right = nullptr, Node* left = nullptr) {
+        this->symbol = symbol;
+        this->frecuency = frecuency;
+        this->right = right;
+        this->left = left;
+        this->parent = nullptr;
+    }
 
-    public:
-        Node(char symbol = '\0', int frecuency = 0, Node* right = nullptr, Node* left = nullptr){
-            this->symbol = symbol;
-            this->frecuency = frecuency;
-            this->right = right;
-            this->left = left;
-            this->parent = nullptr;
-        }
+    /**
+     * Combina dos nodos en un solo nodo.
+     * 
+     * @param u Puntero al primer nodo.
+     * @param v Puntero al segundo nodo.
+     * @return Un nuevo nodo que combina la frecuencia del símbolo de los nodos u y v.
+     */
+    static Node* combine(Node* u, Node* v) {
+        Node* nodo = new Node('\0', u->frecuency + v->frecuency, u, v);
+        u->parent = nodo;
+        v->parent = nodo;
+        return nodo;
+    }
 
-        static Node* combine(Node* u, Node* v) {
-            Node* nodo = new Node('\0', u->frecuency + v->frecuency, u, v);
-            u->parent = nodo;
-            v->parent = nodo;
-            return nodo;
-        }
+    /**
+     * Verifica si el nodo es una hoja.
+     * 
+     * @return true si el nodo es una hoja, false en caso contrario.
+     */
+    bool is_leaf() {
+        return left == nullptr && right == nullptr;
+    }
 
-        bool is_leaf() {
-            return left == nullptr && right == nullptr;
-        }
+    /**
+     * Obtiene el nodo padre.
+     * 
+     * @return Puntero al nodo padre.
+     */
+    Node* getParent() {
+        return parent;
+    }
 
-        Node* getParent() {
-            return parent;
-        }
+    /**
+     * Obtiene el hijo izquierdo.
+     * 
+     * @return Puntero al hijo izquierdo.
+     */
+    Node* getLeft() {
+        return left;
+    }
 
-        Node* getLeft() {
-            return left;
-        }
+    /**
+     * Obtiene el hijo derecho.
+     * 
+     * @return Puntero al hijo derecho.
+     */
+    Node* getRight() {
+        return right;
+    }
 
-        Node* getRight() {
-            return right;
-        }
+    /**
+     * Obtiene el símbolo del nodo.
+     * 
+     * @return El símbolo del nodo.
+     */
+    char getSymbol() {
+        return symbol;
+    }
 
-        char getSymbol() {
-            return symbol;
-        }
+    /**
+     * Obtiene la frecuencia del nodo.
+     * 
+     * @return La frecuencia del nodo.
+     */
+    int getFrecuency() {
+        return frecuency;
+    }
 
-        int getFrecuency() {
-            return frecuency;
-        }
-
-        //Comparador personalizado
-        struct NodeComparator {
-            bool operator()(Node* lhs, Node* rhs) const {
-                if (lhs->getFrecuency() != rhs->getFrecuency()) {
-                    return lhs->getFrecuency() > rhs->getFrecuency();
-                } else {
-                    return lhs->getSymbol() > rhs->getSymbol();
-                }
+    /**
+     * Estructura NodeComparator para comparar dos nodos.
+     */
+    struct NodeComparator {
+        /**
+         * Operador de comparación para los nodos.
+         * 
+         * @param lhs Puntero al primer nodo.
+         * @param rhs Puntero al segundo nodo.
+         * @return true si la frecuencia del primer nodo es mayor que la del segundo nodo, o si las frecuencias son iguales y el símbolo del primer nodo es mayor que el del segundo.
+         */
+        bool operator()(Node* lhs, Node* rhs) const {
+            if (lhs->getFrecuency() != rhs->getFrecuency()) {
+                return lhs->getFrecuency() > rhs->getFrecuency();
+            } else {
+                return lhs->getSymbol() > rhs->getSymbol();
             }
-        };
+        }
+    };
 
-        // bool operator<(const Node& other) const {
-        // if (frecuency != other.frecuency)
-        //     return frecuency < other.frecuency;  // menor a mayor gravedad
-        // else
-        //     return symbol < other.symbol;
-        // }
-
+    // bool operator<(const Node& other) const {
+    // if (frecuency != other.frecuency)
+    //     return frecuency < other.frecuency;  // menor a mayor gravedad
+    // else
+    //     return symbol < other.symbol;
+    // }
 };
 
 
--- Huffman/metodos_huffman.h
+++ Huffman/metodos_huffman.h
@@ -9,116 +9,133 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include "priorityQueueNode.h"
 
 using namespace std;
 
+/*
+    Clase que contiene los métodos de huffman codificar y decodificar, junto con un método que calcula el tamaño aproximado en bytes del mensaje codificado 
+*/
 class metodos_huffman {
 
-    public:
-        static pair<string, Node*> codificar(string& mensaje) {
+public:
+    /**
+     * Codifica un mensaje utilizando el algoritmo de Huffman.
+     * 
+     * @param mensaje El mensaje a codificar.
+     * @return Un par que contiene la cadena codificada y un puntero a la raíz del árbol de Huffman.
+     */
+    static pair<string, Node*> codificar(string& mensaje) {
+        //se ocupa la priority_queue de la STL
+        priority_queue<Node*, vector<Node*>, Node::NodeComparator> cola_p;
+        //se ocupa la tabla hash de la STL
+        unordered_map<char, int> frecuencia_simbolos;
+        unordered_map<char, Node*> hojas;
+        unordered_map<char, string> diccionario;
 
-            // PriorityQueue cola_p;
-            priority_queue<Node*, vector<Node*>, Node::NodeComparator> cola_p;
-            unordered_map<char, int> frecuencia_simbolos;
-            unordered_map<char, Node*> hojas;
-            unordered_map<char, string> diccionario;
-
-            for(char simbolo : mensaje) {
-                frecuencia_simbolos[simbolo]++;
-            }
-
-            for(auto& par : frecuencia_simbolos) {
-                Node* hoja = new Node(par.first, par.second);
-                cola_p.push(hoja);
-                hojas[par.first] = hoja;
-            }
-
-            //si el mensaje está compuesto de un solo simbolo, se codifica la cadena como puros 0, y retorna  el arbol huffman (cola.top)
-            if(cola_p.size() == 1) {
-                string bits = string(mensaje.length(), '0');
-                return make_pair(bits, cola_p.top());
-            }
-
-            while(cola_p.size() > 1) {
-                Node* u = cola_p.top();
-                cola_p.pop();
-                Node* v = cola_p.top();
-                cola_p.pop();
-                Node* nodo_comb = Node::combine(u, v);
-                cola_p.push(nodo_comb);
-                // cout << "nodo 1: " << u->getSymbol() << " "<<  u->getFrecuency() << endl;
-                // cout << "nodo 2: " <<  v->getSymbol() << " " << v->getFrecuency() << endl;
-                // cout << "nodo comb: " <<  nodo_comb->getFrecuency() << endl;
-
-
-            }
-
-            Node* tree = cola_p.top();
-            // cout << "nodo padre: " << tree->getSymbol() << " " <<  tree->getFrecuency() <<endl;
-
-            for(auto& hoja : hojas) {
-                string c;
-                Node* u = hoja.second;
-                //para cada hoja se obtiene su codigo binario
-                while (u != tree) {
-                    Node* parent = u->getParent();  
-                    if(u == parent->getLeft()) {
-                        c += '0';
-                    } else if (u == parent->getRight()) {
-                        c += '1';
-                    }
-                    u = parent;       
-                }
-
-                reverse(c.begin(), c.end());
-                diccionario[hoja.first] = c;
-                // cout << hoja.first << ": " << c << endl;
-            }
-
-
-            string mensaje_cod;
-
-            for(char c : mensaje) {
-                mensaje_cod += diccionario[c];
-            }
-
-            //se retorna el mensaje codificado, junto con el puntero a la raiz del arbol
-            return make_pair(mensaje_cod, tree); 
-
+        // Contar y almacenar la frecuencia de cada símbolo en el mensaje.
+        for (char simbolo : mensaje) {
+            frecuencia_simbolos[simbolo]++;
         }
 
-        static string decodificar(string& mensaje_cod, Node* tree) {
-            string mensaje_decod;
-            if(tree->is_leaf()) {
-                mensaje_decod = string(mensaje_cod.length(), tree->getSymbol());
-                return mensaje_decod;
+        // Crear una hoja para cada símbolo y añadirla a la cola de prioridad.
+        for (auto& par : frecuencia_simbolos) {
+            Node* hoja = new Node(par.first, par.second);
+            cola_p.push(hoja);
+            hojas[par.first] = hoja;
+        }
+
+        // Si el mensaje está compuesto de un solo símbolo, se codifica la cadena como puros 0s y se retorna el árbol de Huffman.
+        if (cola_p.size() == 1) {
+            string bits = string(mensaje.length(), '0');
+            return make_pair(bits, cola_p.top());
+        }
+
+        // Construir el árbol de Huffman combinando nodos.
+        while (cola_p.size() > 1) {
+            Node* u = cola_p.top();
+            cola_p.pop();
+            Node* v = cola_p.top();
+            cola_p.pop();
+            Node* nodo_comb = Node::combine(u, v);
+            cola_p.push(nodo_comb);
+        }
+
+        Node* tree = cola_p.top();
+
+        // Generar el diccionario de códigos binarios para cada hoja.
+        for (auto& hoja : hojas) {
+            string c;
+            Node* u = hoja.second;
+            while (u != tree) {
+                Node* parent = u->getParent();
+                if (u == parent->getLeft()) {
+                    c += '0';
+                } else if (u == parent->getRight()) {
+                    c += '1';
+                }
+                u = parent;
             }
 
-            Node* v = tree;
+            //se invierte para devolver el valor recorriendo el árbol desde la raíz
+            reverse(c.begin(), c.end());
+            diccionario[hoja.first] = c;
+        }
 
-            for(char c : mensaje_cod) {
-                if(c == '0') {
-                    v = v->getLeft();
-                } else {
-                    v = v->getRight();
-                }
+        // Codificar el mensaje utilizando el diccionario.
+        string mensaje_cod;
+        for (char c : mensaje) {
+            mensaje_cod += diccionario[c];
+        }
 
-                if(v->is_leaf()) {
-                    mensaje_decod += v->getSymbol();
-                    v = tree;
-                }
-            }
+        // Retornar el mensaje codificado y el puntero a la raíz del árbol.
+        return make_pair(mensaje_cod, tree);
+    }
 
+    /**
+     * Decodifica un mensaje codificado utilizando el algoritmo de Huffman.
+     * 
+     * @param mensaje_cod El mensaje codificado.
+     * @param tree El puntero a la raíz del árbol de Huffman.
+     * @return El mensaje decodificado.
+     */
+    static string decodificar(string& mensaje_cod, Node* tree) {
+        string mensaje_decod;
+        if (tree->is_leaf()) {
+            mensaje_decod = string(mensaje_cod.length(), tree->getSymbol());
             return mensaje_decod;
         }
 
-        static size_t sizeBytes_mensaje_cod(string& mensaje_comp) {
-            return mensaje_comp.size() / 8;
+        Node* v = tree;
+
+        // Decodificar el mensaje bit a bit recorriendo el árbol de Huffman.
+        for (char c : mensaje_cod) {
+            if (c == '0') {
+                v = v->getLeft();
+            } else {
+                v = v->getRight();
+            }
+
+            if (v->is_leaf()) {
+                mensaje_decod += v->getSymbol();
+                v = tree;
+            }
         }
 
+        return mensaje_decod;
+    }
+
+    /**
+     * Calcula el tamaño en bytes del mensaje codificado.
+     * 
+     * @param mensaje_comp El mensaje codificado.
+     * @return El tamaño en bytes del mensaje codificado.
+     */
+    static size_t sizeBytes_mensaje_cod(string& mensaje_comp) {
+        return mensaje_comp.size() / 8;
+    }
 
 };
+
 
 
 
